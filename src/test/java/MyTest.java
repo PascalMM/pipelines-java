12a4274c9f4fd5984f2f71b3254858b2cf81bf23
PascalMM <PascalMM@users.noreply.github.com>
2020-07-26 16:22:59 +0200

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -11,6 +11,6 @@ public class MyTest {
 
     @Test
     public void test_method_2() {
-        assertTrue("ERROR: Felix muss auch mal ne Azure Pipeline aufsetzen ... ", false);
+        Assert.assertTrue("ERROR: Felix muss auch mal ne Azure Pipeline aufsetzen ... ", false);
     }
 }
